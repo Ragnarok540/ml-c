2ffc8362b0b2bd549fa99a3b40b220156b83f314
Edgar Nova <ragnarok540@gmail.com>
2025-12-29 13:11:27 -0500

    episode 1 - 1:49:00

--- twice.c
+++ twice.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 float train[][2] = {
     {0, 0},
@@ -8,7 +7,11 @@ float train[][2] = {
     {2, 4},
     {3, 6},
     {4, 8},
+    {5, 10},
+    {6, 12},
+    {7, 14},
 };
+
 #define train_count (sizeof(train) / sizeof(train[0]))
 
 float rand_float(void) {
@@ -31,24 +34,23 @@ float cost(float w, float b) {
 
 int main(void) {
     float w = rand_float() * 10.0f;
-    float b = rand_float() * 5.0f;
+    float b = 0; // rand_float() * 5.0f;
     float eps = 1e-3f;
     float rate = 1e-3f;
 
     printf("before: %f\n", cost(w, b));
 
     for (size_t i = 0; i < 500; i++) {
-        float dw = (cost(w + eps, b) - cost(w, b)) / eps;
-        float db = (cost(w, b + eps) - cost(w, b)) / eps;
+        float c = cost(w, b);
+        float dw = (cost(w + eps, b) - c) / eps;
+        float db = (cost(w, b + eps) - c) / eps;
         w -= rate * dw;
         b -= rate * db;
-        printf("cost = %f, w = %f, bias = %f\n", cost(w, b), w, b);
+        printf("cost = %f, w = %f, bias = %f\n", c, w, b);
     }
 
     printf("----------------------------------------------\n");
-    printf("%f\n", w);
-    // printf("Hello C!\n");
+    printf("w = %f, b = %f\n", w, b);
+
     return 0;
 }
-
-// 1 54:00
