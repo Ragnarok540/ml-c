4477bedda21695588cf6439fb3662fd4ceefa9a3
Edgar Nova <ragnarok540@gmail.com>
2026-01-03 19:58:23 -0500

    episode 3 - 1:00:00

--- src/twice.c
+++ src/twice.c
@@ -18,12 +18,12 @@ float rand_float(void) {
     return (float) arc4random() / (float) UINT32_MAX;
 }
 
-float cost(float w, float b) {
+float cost(float w) {
     float result = 0.0f;
 
     for (size_t i = 0; i < train_count; i++) {
         float x = train[i][0];
-        float y = x * w + b;
+        float y = x * w;
         float d = y - train[i][1];
         result += d * d;
     }
@@ -32,25 +32,36 @@ float cost(float w, float b) {
     return result;
 }
 
+float dcost(float w) {
+    float result = 0.0f;
+    for (size_t i = 0; i < train_count; i++) {
+        float x = train[i][0];
+        float y = train[i][1];
+        result += 2 * x * (x * w - y);
+    }
+    result /= train_count;
+    return result;
+}
+
 int main(void) {
     float w = rand_float() * 10.0f;
-    float b = 0; // rand_float() * 5.0f;
-    float eps = 1e-3f;
-    float rate = 1e-3f;
+    float rate = 1e-2f;
+    printf("before: %f\n", cost(w));
 
-    printf("before: %f\n", cost(w, b));
-
-    for (size_t i = 0; i < 500; i++) {
-        float c = cost(w, b);
-        float dw = (cost(w + eps, b) - c) / eps;
-        float db = (cost(w, b + eps) - c) / eps;
+    for (size_t i = 0; i < 50; i++) {
+        float c = cost(w);
+#if 0
+        float eps = 1e-3f;
+        float dw = (cost(w + eps) - c) / eps;
+#else
+        float dw = dcost(w);
+#endif
         w -= rate * dw;
-        b -= rate * db;
-        printf("cost = %f, w = %f, bias = %f\n", c, w, b);
+        printf("cost = %f, w = %f\n", c, w);
     }
 
     printf("----------------------------------------------\n");
-    printf("w = %f, b = %f\n", w, b);
+    printf("w = %f\n", w);
 
     return 0;
 }
